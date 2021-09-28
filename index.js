bd55f4acd41bcc4fd59ab6ad99994b92fdc0fd60
Noel Fernandez <noelfernandez98@gmail.com>
2021-09-28 15:20:33 +0000

    new commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 //1
 function countToFifty(){
-  
+ i = 0
+    for(let i = 0; i <= 50; i++){
+        console.log(i)
+    }
 }
-
+//countToFifty()
 //2
-function countFromOne(){
+function countFromOne(number){
+    for(let i = 0; i <= number; i++)
+    console.log(i)
   
 }
-
+countFromOne(5)
 //3
 function threeAndFiveInFifty(){
   
@@ -19,3 +24,4 @@ function threeAndFive(){
 }
 
 
+console.log(countToFifty)
